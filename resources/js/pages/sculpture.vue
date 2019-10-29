f93c886f0ddc59c626b30b0ebe12d2e775f9df9c
cisoun <cysoun@gmail.com>
2019-10-29 19:38:07 +0100

    Fix last commit

--- resources/js/pages/sculpture.vue
+++ resources/js/pages/sculpture.vue
@@ -6,7 +6,7 @@
         <div class="col-12 text-right">
           <Youtube class="w-100" :url="getVideo(item).url" />
           <div class="title">{{ item.title }}</div>
-          <div v-html="toHTML(item.text)"></div>
+          <div v-html="fastmd(item.text)"></div>
         </div>
       </div>
 
